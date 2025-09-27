05c455063eeb7d954f836909b7eb0215b8529bce
a.gerasimov <a.gerasimov@etpgpb.ru>
2025-09-27 16:14:16 +0300

    Add created_at to User schema to fix 422 error

--- wishspace/backend/schemas.py
+++ wishspace/backend/schemas.py
@@ -87,6 +87,7 @@ class User(BaseModel):
     name: str
     phone: Optional[str] = None
     avatar_url: Optional[str] = None
+    created_at: datetime
 
     class Config:
         from_attributes = True
