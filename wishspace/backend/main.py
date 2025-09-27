ea28336c05ea72fc2294e8dd57229be60b893f8f
a.gerasimov <a.gerasimov@etpgpb.ru>
2025-09-27 17:25:09 +0300

    Fix local_kw error by depending on get_db in get_current_user

--- wishspace/backend/main.py
+++ wishspace/backend/main.py
@@ -45,7 +45,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def get_current_user(db: Session = Depends(SessionLocal), token: str = Depends(oauth2_scheme)):
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
